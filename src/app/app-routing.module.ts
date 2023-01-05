a7f1a1f00f6f02d19a475134265f6238fe4181b9
Pasm-coder <paolasarabia04@gmail.com>
2023-01-05 11:29:16 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessoriesComponent } from './accessories/accessories.component';
+import { HomeComponent } from './home/home.component';
+import { ManComponent } from './man/man.component';
+import { ShoesComponent } from './shoes/shoes.component';
+import { WomenComponent } from './women/women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'mujer', component: WomenComponent},
+  { path: 'hombre', component: ManComponent},
+  { path: 'zapatos', component: ShoesComponent},
+  { path: 'accesorios', component: AccessoriesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
